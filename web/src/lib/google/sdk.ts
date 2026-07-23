87f5a289f10f9ac4edcef45344f63d07511cd8ed
Kurt Thorn <kurt.thorn@gmail.com>
2026-07-22 18:22:05 -0700

    refactor(web): bridge PlaceAutocompleteElement

--- web/src/lib/google/sdk.ts
+++ web/src/lib/google/sdk.ts
@@ -20,7 +20,7 @@ interface MarkerModule {
 }
 
 interface PlacesModule {
-  Autocomplete: Constructor<unknown, [HTMLInputElement, Record<string, unknown>]>;
+  PlaceAutocompleteElement: Constructor<unknown, []>;
 }
 
 interface RoutesModule {
@@ -75,8 +75,8 @@ function createRoutesFacade(modules: RuntimeModules): RoutesFacade {
 
 function createPlacesFacade(modules: RuntimeModules): PlacesFacade {
   return {
-    createAutocomplete(input, options) {
-      return new modules.places.Autocomplete(input, options) as ReturnType<PlacesFacade['createAutocomplete']>;
+    createAutocomplete() {
+      return new modules.places.PlaceAutocompleteElement() as ReturnType<PlacesFacade['createAutocomplete']>;
     },
   };
 }
